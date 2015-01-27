7e256b66f2a1e18a45a257f1f50d46b63d215b82
Duc Nghiem Xuan <nghiem.xuan.duc@framgia.com>
2015-01-27 08:41:32 +0700

    Make the home page

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+
+  def setup
+    @base_title = "Mini Blog"
+  end
+
+  test "should get home" do
+    get :home
+    assert_response :success
+    assert_select "title", @base_title
+  end
+
+end
