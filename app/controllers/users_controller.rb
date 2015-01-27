52d110cfec253c74a0d0bdf373a6b36567c93247
Duc Nghiem Xuan <nghiem.xuan.duc@framgia.com>
2015-01-27 10:41:58 +0700

    Add signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
+
   def new
+    @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # TODO: success -> login user
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :name, :password,
+                                 :password_confirmation)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources 'users'
 
   root 'static_pages#home'
+
+  get 'signup' => 'users#new'
 end
