4b61ab9565ff78bc964ad89d871d08a8f3415407
Duc Nghiem Xuan <nghiem.xuan.duc@framgia.com>
2015-01-28 16:13:09 +0700

    Show feed for new user

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,10 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
-      @feed_items = current_user.feed.paginate(page: params[:page])
+      @feed_items = current_user.feed.paginate(page: params[:page], per_page: 5)
       @entry = current_user.entries.build
     else
-      @feed_items = Entry.all.paginate(page: params[:page])
+      @feed_items = Entry.all.paginate(page: params[:page], per_page: 5)
     end
   end
 end
