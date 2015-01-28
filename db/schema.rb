2e2d1865782f814131a92f64ac0d30be07f50aa7
Duc Nghiem Xuan <nghiem.xuan.duc@framgia.com>
2015-01-28 13:36:43 +0700

    Add index to email in users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150128053357) do
+ActiveRecord::Schema.define(version: 20150128063550) do
 
   create_table "entries", force: :cascade do |t|
     t.text     "content"
@@ -32,5 +32,7 @@ ActiveRecord::Schema.define(version: 20150128053357) do
     t.datetime "updated_at",      null: false
     t.string   "remember_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
